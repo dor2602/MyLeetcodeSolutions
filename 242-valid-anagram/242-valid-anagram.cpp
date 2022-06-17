fa70d85286e8868fcb30052d595cfe7af17cecfc
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-06-18 02:18:31 +0300

    Time: 4 ms (94.78%), Space: 7.4 MB (45.73%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        
         if(s.size() != t.size()) return false;
         int arr[26] = {0};
         for(int i = 0 ; i<s.size() ;i++)
