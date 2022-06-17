f4ebca2ee74fd2f74f16e7457cead6e7e14d016e
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-06-18 02:38:55 +0300

    Time: 55 ms (6.36%), Space: 23.1 MB (76.21%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -3,8 +3,7 @@ public:
     void reverseString(vector<char>& s) 
     {
         int i=0;
-        int j = s.size()-1;
-        while(j > i)
-            swap(s[i++],s[j--]);
+        while(i<(s.size()/2))
+            swap(s[s.size()-1-i],s[i++]);
     }
 };
