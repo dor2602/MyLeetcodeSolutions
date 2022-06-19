dcfa2cd14b4ec63472651b96c73e9947c95a2799
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-06-19 14:54:11 +0300

    Time: 1106 ms (9.22%), Space: 182.9 MB (14.06%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord)
+    {
+        vector<vector<string>>v1;
+        sort(products.begin(), products.end());   
+        int i = 0;
+        string str = "";
+        while (i != searchWord.size())
+        {
+            vector<string>v;
+            int count = 0;
+            str += searchWord[i];
+            for (int j = 0; j < products.size(); j++)
+            {
+                if (products[j].substr(0, str.size()) == str && count < 3)
+                {
+                    count++;
+                    v.push_back(products[j]);
+                }
+            }
+            v1.push_back(v);
+            i++;
+         }
+        return v1;
+     }
+};
