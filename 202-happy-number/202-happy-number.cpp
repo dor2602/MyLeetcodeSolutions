0f1588dab473f694540aaa997d893967e5904aa1
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-06-20 16:13:05 +0300

    Time: 6 ms (24.67%), Space: 6.4 MB (5.75%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_set<int>s;
+    bool isHappy(int n) 
+    {
+        
+        int sum = 0;
+        while(n)
+        {
+            
+            sum += pow(n%10,2);
+            n/=10;
+            
+        }
+        if(sum == 1)
+            return true;
+        if(s.find(sum)!= s.end())
+            return false;
+        s.insert(sum);
+        return isHappy(sum);
+        
+    }
+};
