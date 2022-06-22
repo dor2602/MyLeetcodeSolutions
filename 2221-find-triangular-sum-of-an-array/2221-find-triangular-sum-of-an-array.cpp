dc354dc243b0cdf89da194e7f1c5e41630a6cc16
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-06-22 23:47:57 +0300

    Time: 380 ms (60.20%), Space: 139.9 MB (29.90%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.cpp
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.cpp
@@ -7,12 +7,8 @@ public:
         {
             vector<int>newNums(--n);
             for(int i = 0;i<newNums.size();i++)
-            {
                 newNums[i] = (nums[i] + nums[i+1])%10;
-            }
-            
             nums.resize(n-1);
-            nums.shrink_to_fit();
             nums=newNums;
         }
        return nums[0]; 
