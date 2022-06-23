fce7c56c8eb91960c444042fac66a0cefa0296e6
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-06-23 14:41:47 +0300

    Time: 422 ms (11.04%), Space: 123.3 MB (92.95%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0; i<n; i++)
         if(nums[i]<pivot)
             res[ptr++] = nums[i];
-         ptr = n-1;
+        ptr = n-1;
         for(int i=n-1; i>=0; i--)
             if(nums[i]>pivot)
                 res[ptr--] = nums[i];
