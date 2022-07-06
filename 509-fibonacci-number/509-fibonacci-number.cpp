af7fd5d4397e26bb9845fff31c7f130c30f267d9
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-07-07 00:13:31 +0300

    Time: 0 ms (100.00%), Space: 5.9 MB (80.07%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -9,8 +9,8 @@ public:
         arr[1]=1;
         for(int i=2;i<=n;i++)
         {
-            swap(arr[1],arr[0]);
-            arr[1] = arr[1]+arr[0];
+            arr[1] += arr[0];
+            arr[0] = arr[1]-arr[0];
         }
         return arr[1];
     }
