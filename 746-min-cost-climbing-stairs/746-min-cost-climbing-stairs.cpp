086bd4fb750d622b7f0b0f332e13f09dba697e6a
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-07-06 21:21:39 +0300

    Time: 6 ms (83.23%), Space: 13.6 MB (96.44%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,11 +4,14 @@ public:
     {
         if(cost.size()<3) return cost[0]>cost[1]? cost[1]:cost[0];
         int n=cost.size();
-        int arr[n];
+        int arr[2];
         arr[0]=cost[0];
         arr[1]=cost[1];
         for(int i=2;i<n;i++)
-            arr[i] = min(arr[i-1],arr[i-2])+cost[i];
-        return arr[n-1]>arr[n-2]? arr[n-2]:arr[n-1];
+        {
+             swap(arr[0],arr[1]);
+             arr[1] = min(arr[0],arr[1])+cost[i];
+        }
+        return arr[1]>arr[0]? arr[0]:arr[1];
     }
 };
