a81854eb018e459ad59547db917a94c2872fbc11
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-07-10 00:32:07 +0300

    Time: 93 ms (52.63%), Space: 42.1 MB (86.18%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function(num) {
+    let temp = num;
+    let sum = 0;
+    while(temp>0)
+        {
+            sum+=temp%10;
+            temp = Math.floor(temp/10);
+        }
+    return sum%2 == 0? Math.floor(num/2):Math.ceil(num/2-1);
+    
+};
