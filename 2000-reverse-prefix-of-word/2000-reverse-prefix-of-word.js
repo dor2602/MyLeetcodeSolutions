abc8d25605060e7b7f9874b6b4516f0dce45f910
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-07-10 17:47:07 +0300

    Time: 106 ms (15.15%), Space: 41.8 MB (81.48%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
@@ -5,9 +5,5 @@
  */
 var reversePrefix = function(word, ch) 
 { 
-   // if(!word.indexOf(ch))
-       // return word;
    return word.split("").slice(0,word.indexOf(ch)+1).reverse().join("")+word.slice(word.indexOf(ch)+1);
-    
-    
 };
