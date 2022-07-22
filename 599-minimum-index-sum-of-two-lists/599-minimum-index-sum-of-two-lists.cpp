7749f307abf5c27c64d9aa0e1b28271c0271e629
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-07-22 18:26:06 +0300

    Time: 91 ms (92.85%), Space: 37.5 MB (34.33%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
@@ -3,10 +3,10 @@ public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) 
     {
         unordered_map<string,int>list;
+        unordered_map<int,vector<string>>sumIndexes;
         for(int i=0; i<list1.size() ;i++)
             list[list1[i]] = i;
         int indexS = INT_MAX;
-        unordered_map<int,vector<string>>v ;
         for(int i=0;i<list2.size();i++)
         {
             if(list.find(list2[i]) != list.end())
@@ -14,10 +14,10 @@ public:
                 if(list[list2[i]]+i <= indexS)
                 {
                     indexS = list[list2[i]]+i;
-                    v[indexS].push_back(list2[i]);
+                    sumIndexes[indexS].push_back(list2[i]);
                 }
             }
         }
-        return v[indexS];
+        return sumIndexes[indexS];
     }
 };
