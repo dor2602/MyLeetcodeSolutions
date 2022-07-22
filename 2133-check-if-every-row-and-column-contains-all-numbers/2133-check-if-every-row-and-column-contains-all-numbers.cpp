1a106b1a89c8f5bcce911a95032761aec7132674
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-07-22 16:42:43 +0300

    Time: 599 ms (18.50%), Space: 68.1 MB (56.91%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -3,16 +3,17 @@ public:
     bool checkValid(vector<vector<int>>& matrix) 
     {
         int n=matrix.size();
-        
+        unordered_set<int>row,col;
         for(int i=0;i<n;i++)
         {
-            unordered_set<int>row,col;
+            row.clear();
+            col.clear();
             for(int j=0;j<n;j++)
             {
                 row.insert(matrix[i][j]);
                 col.insert(matrix[j][i]);
             }
-            if(col.size()!= n || row.size()!=n)
+            if((col.size()+row.size())!= 2*n)
                 return false;
         }
         return true;
