82aeec0912d5883eb81581c1a29c2cf16950000b
dor2602 <80360729+dor2602@users.noreply.github.com>
2022-08-04 14:45:43 +0300

    Time: 29 ms (77.76%), Space: 19.1 MB (83.25%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) 
     {
-        int j=0;
-        for(int i = 0;i<nums.size();i++)
-        {
+        int index=0;
+        for(int i=0;i<nums.size();i++)
             if(nums[i]!=0)
-            {
-               nums[j] = nums[i];
-                j++;
-            }   
-        }
-        for(int i = j;i<nums.size();i++)
-            nums[i] = 0;
-    } 
+                nums[index++]=nums[i];
+        for(int i=index;i<nums.size();i++)
+            nums[i]=0;
+    }
 };
